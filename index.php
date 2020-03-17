ccda8610f642c4652ef3b33099754d92e40e346b
wac <daisy.bruneau@epitech.eu>
2020-03-17 17:47:55 +0100

    .

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- BASICS 
+    ______________________________-->
+
+    <meta charset="UTF-8">
+    <title>My_H5ai</title>
+
+    <!-- CSS 
+    ______________________________-->
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- MOBILE SPECIFIC METAS 
+    ______________________________-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- FONT 
+    ______________________________-->
+
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+    <!-- FAVICON 
+    ______________________________-->
+
+    <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+
+<body>
+
+<header>
+    <section class="container-fluid">
+        <div class="row">
+            <div class="header">
+                <h1>My_H5ai</h1>
+            </div>    
+        </div>
+        <div class="row">
+            <div>
+                <?php echo $path_directory ?>
+            </div>
+        </div>
+    </section>
+</header>
+<div class="row">
+    <section class="container-fluid">
+        <section class="col-3 menunav">
+                <!-- AFFICHE TOUS LES DOSSIERS COURANTS -->
+                <?php
+                $path_directory = "/home/wac/daisyB-repo/";
+                $array = scandir($path_directory);
+                // var_dump($array);
+                
+                for ($i = 0 ; $i < sizeof($array); $i++)
+                {
+                    echo "<a href='explore.php'><li>". $array[$i] . PHP_EOL . "</li></a>"; 
+                }
+                
+                ?>
+        </section>
+    
+    <section class="col-9 explorator">
+            <!-- AFFICHE LE CONTENU DU DOSSIER SELECTIONNE -->
+    </section>
+</div>
+
+</section>
+
+
+
+
+
+<!-- <?php 
+// ================ SCANDIR ================
+
+$path_directory = "/home/wac/daisyB-repo";
+    
+    $dir = basename($path_directory);
+    echo "<h3>/".$dir."</h3>";
+    
+    $array = scandir($path_directory);
+    // var_dump($array);
+    
+    for ($i = 0 ; $i < sizeof($array); $i++)
+    {
+        echo "<li><a>". $array[$i] . "</a></li>"; 
+    }
+
+function scan($select)
+{
+
+    $path_directory = $select;
+
+
+    $array = scandir($path_directory);
+    // var_dump($array);
+    
+    for ($i = 0 ; $i < sizeof($array); $i++)
+    {
+        echo "<li><a>". $array[$i] . "</a></li>"; 
+    }
+}
+
+
+?> -->
+
+</body>
+</html>
